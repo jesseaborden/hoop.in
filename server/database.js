1a1921608085aadd233d24f7cd1d5475e652a7f0
jesse <jesseaborden@gmail.com>
2016-10-23 22:43:53 -0700

    fixed db file

--- server/database.js
+++ server/database.js
@@ -1,18 +1,6 @@
 var Sequelize = require('sequelize');
 
-<<<<<<< e43a63fadc202e1a92dbeded1b6ca9d1bd4eb70d
-<<<<<<< 87e68c2847aeb053d931bedb843b0a360e8126d6
 var sequelize = new Sequelize('postgres://nhhfsvxl:PmfmyAew57jYcGCbVPbLg2A2n13rEyXD@elmer.db.elephantsql.com:5432/nhhfsvxl');
-=======
-var sequelize = new Sequelize('postgres://eghnhvmy:4Ux6rwaZSerOqXvyNIX6sgnfVfftpz4B@elmer.db.elephantsql.com:5432/eghnhvmy');
->>>>>>> [fixed]-Using my database url
-=======
-<<<<<<< 6b79b09f83170ff5d880dd3e38d95b989d4937d5
-var sequelize = new Sequelize('postgres://eghnhvmy:4Ux6rwaZSerOqXvyNIX6sgnfVfftpz4B@elmer.db.elephantsql.com:5432/eghnhvmy');
-=======
-var sequelize = new Sequelize('postgres://nhhfsvxl:PmfmyAew57jYcGCbVPbLg2A2n13rEyXD@elmer.db.elephantsql.com:5432/nhhfsvxl');
->>>>>>> commiting before rebase
->>>>>>> fixed merge conflict
 
 module.exports = {
   sequelize: sequelize,
