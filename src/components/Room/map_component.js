7a32f41ceaa6def64b63c064405565f4030468db
jesse <jesseaborden@gmail.com>
2016-10-24 12:07:41 -0700

    [add]-set up google maps on the room page with marker

--- src/components/Room/map_component.js
+++ src/components/Room/map_component.js
@@ -1,11 +1,27 @@
 import React from 'react';
 
-const Gmap = props => (
+const  lat = localStorage.getItem("lat");
+const  lng = localStorage.getItem("lng");
+const map = `https://www.google.com/maps/embed/v1/place?key=AIzaSyBlnC4Cp2iBrYCbpQ4jLEYIhD4_MGQXxd4&q=${lat},${lng}&zoom=12`;
+
+class Gmap extends React.Component {
+  constructor (props) {
+    super(props),
+    this.state = {lat: 0, lng: 0, map:""};
+  }
+
+  componentWillMount() {
+  	this.setState({"lat": localStorage.getItem("lat"), "lng": localStorage.getItem("lng"), map: `https://www.google.com/maps/embed/v1/place?key=AIzaSyBlnC4Cp2iBrYCbpQ4jLEYIhD4_MGQXxd4&q=${lat},${lng}&zoom=12`});
+  }
+
+render(){
+	return(
 <div>
-	<iframe className="center-block" src="https://www.google.com/maps/embed/v1/place?key=AIzaSyBlnC4Cp2iBrYCbpQ4jLEYIhD4_MGQXxd4&q=Space+Needle,Seattle+WA"></iframe>
+	<iframe id="map" className="center-block" src={map}></iframe>
 		<button className="center-block">end game</button>
 		<button className="center-block">leave game</button>
  </div>
-);
-
+ )
+}
+}
 export default Gmap;
