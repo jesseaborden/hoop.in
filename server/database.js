ca1540fb042a78d0b66b773d42663db31b36a8d5
jesse <jesseaborden@gmail.com>
2016-10-23 22:39:26 -0700

    [fix]-fixed conflict

--- server/database.js
+++ server/database.js
@@ -1,6 +1,10 @@
 var Sequelize = require('sequelize');
 
+<<<<<<< 87e68c2847aeb053d931bedb843b0a360e8126d6
 var sequelize = new Sequelize('postgres://nhhfsvxl:PmfmyAew57jYcGCbVPbLg2A2n13rEyXD@elmer.db.elephantsql.com:5432/nhhfsvxl');
+=======
+var sequelize = new Sequelize('postgres://eghnhvmy:4Ux6rwaZSerOqXvyNIX6sgnfVfftpz4B@elmer.db.elephantsql.com:5432/eghnhvmy');
+>>>>>>> [fixed]-Using my database url
 
 module.exports = {
   sequelize: sequelize,
